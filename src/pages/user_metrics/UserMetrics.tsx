a30d88a6779cc34f6e4a06cb7b73a533b7ceee5c
Lucas <lgrati@fi.uba.ar>
2023-12-05 19:20:08 -0300

    iframe list setup

--- src/pages/user_metrics/UserMetrics.tsx
+++ src/pages/user_metrics/UserMetrics.tsx
@@ -1,11 +1,81 @@
 import React from "react";
 import "./user_metrics.scss";
 
+interface IframeInfo {
+  src: string;
+  title: string;
+  description: string;
+}
+
+interface IframeListProps {
+  iframes: IframeInfo[];
+}
+
+const IframeList: React.FC<IframeListProps> = ({ iframes }) => {
+  return (
+    <div style={{ overflowX: 'auto', overflowY: 'hidden', width: '100%', boxSizing: 'border-box' }}>
+      <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%' }}>
+        {iframes.map((iframe, index) => (
+          <div key={index} style={{ width: '50%', padding: '10px', boxSizing: 'border-box' }}>
+            <h3>{iframe.title}</h3>
+            <p>{iframe.description}</p>
+            <iframe src={iframe.src} width="100%" height="305" title={iframe.title}></iframe>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const iframes: IframeInfo[] = [
+  {
+    src: "https://us5.datadoghq.com/graph/embed?token=e17b70304afbf1ed288f4ad6bce9d854443daa80315fbc4979c2d7490ae6c596&height=300&width=600&legend=true",
+    title: "Title 1",
+    description: "Description 1",
+  },
+  {
+    src: "https://us5.datadoghq.com/graph/embed?token=e17b70304afbf1ed288f4ad6bce9d854443daa80315fbc4979c2d7490ae6c596&height=300&width=600&legend=true",
+    title: "Title 2",
+    description: "Description 2",
+  },
+  {
+    src: "https://us5.datadoghq.com/graph/embed?token=e17b70304afbf1ed288f4ad6bce9d854443daa80315fbc4979c2d7490ae6c596&height=300&width=600&legend=true",
+    title: "Title 1",
+    description: "Description 1",
+  },
+  {
+    src: "https://us5.datadoghq.com/graph/embed?token=e17b70304afbf1ed288f4ad6bce9d854443daa80315fbc4979c2d7490ae6c596&height=300&width=600&legend=true",
+    title: "Title 1",
+    description: "Description 1",
+  },
+  {
+    src: "https://us5.datadoghq.com/graph/embed?token=e17b70304afbf1ed288f4ad6bce9d854443daa80315fbc4979c2d7490ae6c596&height=300&width=600&legend=true",
+    title: "Title 1",
+    description: "Description 1",
+  },
+  {
+    src: "https://us5.datadoghq.com/graph/embed?token=e17b70304afbf1ed288f4ad6bce9d854443daa80315fbc4979c2d7490ae6c596&height=300&width=600&legend=true",
+    title: "Title 1",
+    description: "Description 1",
+  },
+  {
+    src: "https://us5.datadoghq.com/graph/embed?token=e17b70304afbf1ed288f4ad6bce9d854443daa80315fbc4979c2d7490ae6c596&height=300&width=600&legend=true",
+    title: "Title 1",
+    description: "Description 1",
+  },
+  {
+    src: "https://us5.datadoghq.com/graph/embed?token=e17b70304afbf1ed288f4ad6bce9d854443daa80315fbc4979c2d7490ae6c596&height=300&width=600&legend=true",
+    title: "Title 1",
+    description: "Description 1",
+  }
+];
+
 export const UserMetrics = () => {
   return (
     <div className="user_metrics">
-      <div style={{ marginBottom: "1%" }}>
+      <div>
         <h1 className="title">User Analytics</h1>
+        <IframeList iframes={iframes} />
       </div>
     </div>
   );
